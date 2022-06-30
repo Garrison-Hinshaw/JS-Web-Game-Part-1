6e6cc6d475d039adb28a53eaa3334382751e8d33
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-06-30 19:01:46 -0400

    Added function for images that are included

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function newImage
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(link, left, bottom){
+    let picture = document.createElement('img')
+    picture.src = url
+    picture.style.position = 'fixed'
+    picture.style.left = '100px'
+    picture.style.bottom = '100px'
+    document.body.append(picture)
+    return picture
+}
 
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
