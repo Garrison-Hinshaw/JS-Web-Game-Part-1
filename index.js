efa67b5b85fdf661c0b86ea0f6bea5f368f4344e
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-06-30 22:03:18 -0400

    added newItem function and newItem images

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function newImage(link, left, bottom){
     return image
 }
 
+function newItem(link, left, bottom){
+    let image = newImage(link, left, bottom)
+
+    link.addEventListener('dblclick', ()=> {
+        link.remove()
+    })
+}
 
 newImage('assets/green-character.gif', 100, 100);
 newImage('assets/pine-tree.png', 450, 200);
@@ -15,4 +22,6 @@ newImage('assets/tree.png', 200, 300);
 newImage('assets/pillar.png', 350, 100);
 newImage('assets/crate.png', 150, 300);
 newImage('assets/well.png', 500, 425);
-
+newItem('assets/sword.png', 500, 405);
+newItem('assets/shield.png', 165, 185);
+newItem('assets/staff.png', 600, 100);
